320e004b9baaa52fe829f282155fe81cb7eeb923
valentina buoro <valentinabuoro4@gmail.com>
2024-02-20 22:30:17 +0100

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(colors: [Colors.blue, Colors.indigo]),
-        ),
-        child: const StartScreen(),
-      ),
-    ),
-  ));
+  runApp(const Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/start_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() => _QuizState();
+}
+
+class _QuizState extends State<Quiz> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [Colors.blue, Colors.indigo]),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    );
+  }
+}
